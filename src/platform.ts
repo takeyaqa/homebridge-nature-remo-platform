dcf6104c745703469f092b523b160b6f2c2406cc
Takeshi Kishi <takeya0x86@gmail.com>
2021-02-27 17:17:29 +0900

    refactor platform class

--- src/platform.ts
+++ src/platform.ts
@@ -32,7 +32,12 @@ export class NatureRemoPlatform implements DynamicPlatformPlugin {
 
     this.api.on(APIEvent.DID_FINISH_LAUNCHING, () => {
       logger.debug('Executed didFinishLaunching callback');
-      this.discoverDevices();
+      this.discoverDevices().then(() => {
+        logger.info('Completed discover accessories');
+      }).catch((err) => {
+        logger.error(err.message);
+        throw err;
+      });
     });
   }
 
@@ -41,52 +46,44 @@ export class NatureRemoPlatform implements DynamicPlatformPlugin {
     this.accessories.push(accessory);
   }
 
-  discoverDevices(): void {
-    this.natureRemoApi.getAllAppliances().then((appliances) => {
-      for (const appliance of appliances) {
-        if (appliance.type === 'LIGHT' || appliance.type === 'AC') {
-          const uuid = appliance.id;
-          const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
-          if (existingAccessory) {
-            this.logger.info('Restoring existing accessory from cache:', existingAccessory.displayName);
-            if (appliance.type === 'LIGHT') {
-              new NatureNemoLightAccessory(this, existingAccessory);
-            } else if (appliance.type === 'AC') {
-              new NatureNemoAirConAccessory(this, existingAccessory);
-            }
-          } else {
-            this.logger.info('Adding new accessory:', appliance.nickname);
-            const accessory = new this.api.platformAccessory(appliance.nickname, uuid);
-            accessory.context = { appliance: appliance };
-            if (appliance.type === 'LIGHT') {
-              new NatureNemoLightAccessory(this, accessory);
-            } else if (appliance.type === 'AC') {
-              new NatureNemoAirConAccessory(this, accessory);
-            }
-            this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+  async discoverDevices(): Promise<void> {
+    const devices = await this.natureRemoApi.getAllDevices();
+    for (const device of devices) {
+      const existingAccessory = this.accessories.find(accessory => accessory.UUID === device.id);
+      if (existingAccessory) {
+        this.logger.info('Restoring existing accessory from cache:', existingAccessory.displayName);
+        new NatureNemoSensorAccessory(this, existingAccessory);
+      } else {
+        this.logger.info('Adding new accessory:', device.name);
+        const accessory = new this.api.platformAccessory(device.name, device.id);
+        accessory.context = { device: device };
+        new NatureNemoSensorAccessory(this, accessory);
+        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+      }
+    }
+    const appliances = await this.natureRemoApi.getAllAppliances();
+    for (const appliance of appliances) {
+      if (appliance.type === 'LIGHT' || appliance.type === 'AC') {
+        const existingAccessory = this.accessories.find(accessory => accessory.UUID === appliance.id);
+        if (existingAccessory) {
+          this.logger.info('Restoring existing accessory from cache:', existingAccessory.displayName);
+          if (appliance.type === 'LIGHT') {
+            new NatureNemoLightAccessory(this, existingAccessory);
+          } else if (appliance.type === 'AC') {
+            new NatureNemoAirConAccessory(this, existingAccessory);
           }
+        } else {
+          this.logger.info('Adding new accessory:', appliance.nickname);
+          const accessory = new this.api.platformAccessory(appliance.nickname, appliance.id);
+          accessory.context = { appliance: appliance };
+          if (appliance.type === 'LIGHT') {
+            new NatureNemoLightAccessory(this, accessory);
+          } else if (appliance.type === 'AC') {
+            new NatureNemoAirConAccessory(this, accessory);
+          }
+          this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
         }
       }
-    }).then(() => {
-      this.natureRemoApi.getAllDevices().then((devices) => {
-        for (const device of devices) {
-          const uuid = device.id;
-          const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
-          if (existingAccessory) {
-            this.logger.info('Restoring existing accessory from cache:', existingAccessory.displayName);
-            new NatureNemoSensorAccessory(this, existingAccessory);
-          } else {
-            this.logger.info('Adding new accessory:', device.name);
-            const accessory = new this.api.platformAccessory(device.name, uuid);
-            accessory.context = { device: device };
-            new NatureNemoSensorAccessory(this, accessory);
-            this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
-          }
-        }
-      });
-    }).catch((err) => {
-      this.logger.error(err.message);
-      throw err;
-    });
+    }
   }
 }
