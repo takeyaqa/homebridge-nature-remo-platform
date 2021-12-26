b59363b541e6314c35c6393a469932eaa43fcb34
Takeshi Kishi <takeya0x86@icloud.com>
2021-12-27 04:35:47 +0900

    refactor light accessory (#17)

--- src/lightAccessory.ts
+++ src/lightAccessory.ts
@@ -1,12 +1,4 @@
-import {
-  CharacteristicEventTypes,
-  CharacteristicGetCallback,
-  CharacteristicSetCallback,
-  CharacteristicValue,
-  PlatformAccessory,
-  Service,
-} from 'homebridge';
-
+import { CharacteristicValue, PlatformAccessory, Service } from 'homebridge';
 import { NatureRemoPlatform } from './platform';
 
 export class NatureNemoLightAccessory {
@@ -18,45 +10,51 @@ export class NatureNemoLightAccessory {
     private readonly platform: NatureRemoPlatform,
     private readonly accessory: PlatformAccessory,
   ) {
+    this.accessory.category = this.platform.api.hap.Categories.LIGHTBULB;
+
     this.accessory.getService(this.platform.Service.AccessoryInformation)!
-      .setCharacteristic(this.platform.Characteristic.Manufacturer, accessory.context.appliance.model.manufacturer)
-      .setCharacteristic(this.platform.Characteristic.Model, accessory.context.appliance.model.name)
-      .setCharacteristic(this.platform.Characteristic.SerialNumber, accessory.context.appliance.id);
+      .setCharacteristic(this.platform.Characteristic.Manufacturer, this.accessory.context.appliance.model.manufacturer)
+      .setCharacteristic(this.platform.Characteristic.Model, this.accessory.context.appliance.model.name)
+      .setCharacteristic(this.platform.Characteristic.SerialNumber, this.accessory.context.appliance.id)
+      .setCharacteristic(this.platform.Characteristic.Name, this.accessory.context.appliance.nickname);
 
     this.service = this.accessory.getService(this.platform.Service.Lightbulb) || this.accessory.addService(this.platform.Service.Lightbulb);
-    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.appliance.nickname);
     this.service.getCharacteristic(this.platform.Characteristic.On)
-      .on(CharacteristicEventTypes.GET, this.getOn.bind(this))
-      .on(CharacteristicEventTypes.SET, this.setOn.bind(this));
+      .onGet(this.getOn.bind(this))
+      .onSet(this.setOn.bind(this));
 
-    this.platform.logger.debug('[%s] id -> %s', accessory.context.appliance.nickname, accessory.context.appliance.id);
-    this.name = accessory.context.appliance.nickname;
-    this.id = accessory.context.appliance.id;
+    this.platform.logger.debug('[%s] id -> %s', this.accessory.context.appliance.nickname, this.accessory.context.appliance.id);
+    this.name = this.accessory.context.appliance.nickname;
+    this.id = this.accessory.context.appliance.id;
   }
 
-  getOn(callback: CharacteristicGetCallback): void {
+  async getOn(): Promise<CharacteristicValue> {
     this.platform.logger.debug('getOn called');
-    this.platform.natureRemoApi.getLightState(this.id).then((lightState) => {
+    try {
+      const lightState = await this.platform.natureRemoApi.getLightState(this.id);
       this.platform.logger.info('[%s] On -> %s', this.name, lightState.on);
-      callback(null, lightState.on);
-    }).catch((err) => {
-      this.platform.logger.error(err.message);
-      callback(err);
-    });
+      return lightState.on;
+    } catch (err) {
+      if (err instanceof Error) {
+        this.platform.logger.error(err.message);
+      }
+      throw err;
+    }
   }
 
-  setOn(value: CharacteristicValue, callback: CharacteristicSetCallback): void {
+  async setOn(value: CharacteristicValue): Promise<void> {
     this.platform.logger.debug('setOn called ->', value);
     if (typeof value !== 'boolean') {
-      callback(new Error('value must be a boolean'));
-      return;
+      throw new Error('value must be a boolean');
     }
-    this.platform.natureRemoApi.setLight(this.id, value).then(() => {
+    try {
+      await this.platform.natureRemoApi.setLight(this.id, value);
       this.platform.logger.info('[%s] On <- %s', this.name, value);
-      callback(null);
-    }).catch((err) => {
-      this.platform.logger.error(err.message);
-      callback(err);
-    });
+    } catch (err) {
+      if (err instanceof Error) {
+        this.platform.logger.error(err.message);
+      }
+      throw err;
+    }
   }
 }
